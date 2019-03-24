b30dfcee150d160d072a34768cf6164cd4105c92
bgreni <42788181+bgreni@users.noreply.github.com>
2019-03-23 20:41:13 -0600

    did a bunch of stuff

--- source.py
+++ source.py
@@ -1,53 +1,291 @@
-import pandas
+import pandas as pd
 import folium
+import sqlite3
+import matplotlib.pyplot as plt
+import tkinter as tk
+import webbrowser
+
+
+
 
 
 class UI:
-    def __init__(self):
-        pass
-    
+    def __init__(self,root):
+        # init all instance attributes
+        
+        self.db = Database()
+        # get GUI root
+        self.win = root
+        self.win.title("Crime Stats")
+        # counters for naming purpose when saving results
+        self.Q1Counter = 0
+        self.Q2Counter = 0
+        self.Q3Counter = 0
+        self.Q4Counter = 0
+        
+       
     def run(self):
-        while True:
-            choice = self.showMainMenu()
-            if choice == '1':
-                pass
-            elif choice == '2':
-                pass
-            elif choice == '3':
-                pass
-            elif choice == '4':
-                pass
-            elif choice.lower() == 'e':
-                break
-            else:
-                print("Choice is invalid")            
+        self.buildMainMenu()    
     
-    def showMainMenu(self):
-        print("Please make a selection")
-        options = ["1: Q1","2: Q2","3: Q3","4: Q4","E: Exit"]
-        for option in options:
-            print(option)
-        choice = input("Enter your choice: ")
-        return choice
+    def clearRoot(self):
+        # clear the window of the current frame
+        
+        for el in root.winfo_children():
+            el.destroy()
+            
+    def buildFrame(self,contents):
+        # adds contents to parent frame
+        # contents is a list of UI elements
+        
+        for item in contents:
+            # pack from the top down
+            item.pack(side="top")
+    
+    def buildMainMenu(self):
+        # builds the main menu UI
+        
+        # clear window
+        self.clearRoot()
+        # delcare a frame to hold menu contents
+        self.mainFrame = tk.Frame(self.win)
+        self.mainFrame.pack(expand=True,fill="both")
+        # add top label
+        prompt = "Please make a selection"
+        self.mainLabel = tk.Label(self.mainFrame,text=prompt)
+        # define main menu buttons
+        self.Q1Button = tk.Button(self.mainFrame,text="1: Q1",command=lambda: self.buildBarPlotMenu())
+        self.Q2Button = tk.Button(self.mainFrame,text="2: Q2")
+        self.Q3Button = tk.Button(self.mainFrame,text="3: Q3")
+        self.Q4Button = tk.Button(self.mainFrame,text="4: Q4",command=lambda: self.buildQ4Menu())
+        # build the frame
+        self.mainMenuContents = [self.mainLabel,self.Q1Button,self.Q2Button,self.Q3Button,self.Q4Button]
+        self.buildFrame(self.mainMenuContents)
+    
+    
+        
+    def getbpEntries(self):
+        # retrieves and validates the user input, and generates the bar plot
+        
+        try:
+            # get the user input
+            lower = int(self.beLowerText.get())
+            upper = int(self.beUpperText.get())
+            # get a list of all acceptable crime types
+            cTypes = self.db.getCrimeTypes()
+            crime = self.beCrimeText.get()
+            
+            # check for valid crime type
+            if not crime in cTypes.values:
+                raise ValueError
+            
+            # current menu is no longer needed, recreate main menu
+            self.buildMainMenu()
+            
+            # get crime frequencies
+            data = self.db.getCrimeByMonth(crime,lower,upper)
+            if data.empty:
+                raise ValueError
+            
+            # plot graph and save png to local folder
+            plot = data.plot.bar(x="Month")
+            plt.plot()
+            self.Q1Counter += 1
+            plt.savefig("Q1-{}.png".format(self.Q1Counter))            
+            plt.show() 
+        
+        # alert the user to invalid input    
+        except ValueError:
+            tk.messagebox.showerror("Error","Invalid input given")
+            
+    
+    
+    def buildBarPlotMenu(self):
+        # builds the bar plot menu UI
+        
+        # clear window
+        self.clearRoot()
+        # declare new frame
+        self.barEntryFrame = tk.Frame(self.win)
+        self.barEntryFrame.pack(expand=True,fill="both")
+        # add top label
+        prompt = "Enter a year range and crime type"
+        self.topLabel = tk.Label(self.barEntryFrame,text=prompt)
+        # add text entry boxes with labels for all needed information
+        self.beUpperLabel = tk.Label(self.barEntryFrame,text="Year Upper Bound:")
+        self.beUpperText = tk.Entry(self.barEntryFrame)
+        self.beLowerLabel = tk.Label(self.barEntryFrame,text="Year Lower Bound")
+        self.beLowerText = tk.Entry(self.barEntryFrame)
+        self.beCrimeLabel = tk.Label(self.barEntryFrame,text="Crime Type")
+        self.beCrimeText = tk.Entry(self.barEntryFrame)
+        # add confirm button
+        self.beConfirm = tk.Button(self.barEntryFrame,text="Confirm",command=lambda: self.getbpEntries())
+        # build frame
+        self.beMenuContents = [self.topLabel,self.beUpperLabel,self.beUpperText,
+                               self.beLowerLabel,self.beLowerText,self.beCrimeLabel,
+                               self.beCrimeText,self.beConfirm]
+        self.buildFrame(self.beMenuContents)
+    
+ 
+            
+    
+    def buildQ4Menu(self):
+        # buils Q4 menu UI
+        
+        # clear window
+        self.clearRoot()
+        # declare a new frame
+        self.Q4EntryFrame = tk.Frame(self.win)
+        self.Q4EntryFrame.pack(expand=True,fill="both")
+        # add top label
+        prompt = "Enter a year range and number of neighbourhoods"
+        self.Q4topLabel = tk.Label(self.Q4EntryFrame,text=prompt)
+        # add text entry fields with labels
+        self.Q4UpperLabel = tk.Label(self.Q4EntryFrame,text="Year Upper Bound:")
+        self.Q4UpperText = tk.Entry(self.Q4EntryFrame)
+        self.Q4LowerLabel = tk.Label(self.Q4EntryFrame,text="Year Lower Bound")
+        self.Q4LowerText = tk.Entry(self.Q4EntryFrame)
+        self.Q4NLabel = tk.Label(self.Q4EntryFrame,text="Top N Neighborhoods")
+        self.Q4NText = tk.Entry(self.Q4EntryFrame)
+        # add confirm button
+        self.Q4Confirm = tk.Button(self.Q4EntryFrame,text="Confirm",command=lambda: self.getQ4Entries())
+        # build the menu
+        self.Q4MenuContents = [self.Q4topLabel,self.Q4UpperLabel,self.Q4UpperText,
+                               self.Q4LowerLabel,self.Q4LowerText,self.Q4NLabel,
+                               self.Q4NText,self.Q4Confirm]
+        self.buildFrame(self.Q4MenuContents)
+        
+    
+    def getQ4Entries(self):
+        # get the user input then render and output the map
+        
+        try:
+            # get user input
+            lower = int(self.Q4LowerText.get())
+            upper = int(self.Q4UpperText.get())
+            N = int(self.Q4NText.get())
+            # init the map
+            m = folium.Map(location=[53.5444,-113.323],zoom_start=11)
+            
+            # get top N areas in the given range
+            data = self.db.getQ4Info(lower,upper,N)
+            if data.empty:
+                raise ValueError            
+            
+            # add a circle on the map for every area of interest
+            for i in range(N):
+                row = data.iloc[i]
+                name = row["name1"]
+                ratio = float(row["ratio"])
+                long = float(row["long"])
+                lat = float(row["lat"])
+                # get the most common crime for the current area
+                commonCrime = self.db.getMostCommonCrimeType(name,lower,upper)["cType"][0]
+                # create and add the circle to the map
+                folium.Circle(
+                    location=[lat,long],
+                    popup="{} <br> {} <br> {}".format(name,commonCrime,ratio),
+                    radius=3000*ratio,
+                    fill=True,
+                    fill_color="crimson"
+                ).add_to(m)
+            
+            # save the hmtl to the local folder
+            self.Q4Counter += 1
+            filename = "Q4-{}.html".format(self.Q4Counter)
+            m.save(filename)
+            # open map in default web browser
+            webbrowser.open(filename,new=2)
+            # return to main menu
+            self.buildMainMenu()
+                    
+        except ValueError:
+            tk.messagebox.showerror("Error","Invalid input given")       
     
     
 class Database:
     def __init__(self):
+        # init instance variables for database usage
+        
         self.db_path = 'a4-sampled.db'
         self.connection = sqlite3.connect(self.db_path)
         self.cursor = self.connection.cursor()        
     
-    def getCrimeByMonth(self,crimeType):
-        query = '''SELECT Month,Count(*) as number
+    
+    def getCrimeByMonth(self,crimeType,lower,upper):
+        # get crime frequency by month for a given range of years for a given crime type
+        # crimeType is a the crime type of interest
+        # lower is the lower bound year
+        # upper is the upper bound year
+        
+        query = '''SELECT Month,Count(CASE WHEN Crime_Type = '{}' THEN 1 ELSE NULL END) as number
                    FROM crime_incidents
-                   WHERE Crime_Type = '{}'
-                   GROUP BY Month'''.format(crimeType)
+                   WHERE Year <= {} AND Year >= {}
+                   GROUP BY Month'''.format(crimeType,upper,lower)
+        
+        result = pd.read_sql_query(query,self.connection)
+        return result
+    
+    def getCrimeTypes(self):
+        # gets all possible crime types in the database
+        
+        query = '''SELECT Crime_Type
+                   FROM crime_incidents
+                   GROUP BY Crime_Type'''
+        result = pd.read_sql_query(query,self.connection)
+        return result["Crime_Type"]
+    
+    def getQ4Info(self,lower,upper,limit):
+        # gets the name, crime to population ratio and coordinates of the top N relevant areas in a given year range
+        # lower is the lower bound year
+        # upper is the upper bound year
+        # limit a number for the top N areas to look at
+        
+        query = '''SELECT name1, (crimeNum*1.0/popNum) as ratio, c.longitude as long,
+                   c.latitude as lat
+                   FROM (
+                         SELECT Neighbourhood_Name as name1, COUNT(*) as crimeNum
+                         FROM crime_incidents
+                         WHERE Year <= {} AND Year >= {}
+                         GROUP BY Neighbourhood_Name),
+                         (SELECT Neighbourhood_Name as name2,(NO_RESPONSE+CANADIAN_CITIZEN+NON_CANADIAN_CITIZEN) as popNum
+                         FROM population
+                         WHERE popNum != 0
+                         GROUP BY Neighbourhood_Name),
+                         coordinates c
+                    WHERE name1 = name2 AND name1 = c.Neighbourhood_Name
+                    AND long != 0 AND lat != 0
+                    ORDER BY ratio DESC
+                    LIMIT {}'''.format(upper,lower,limit)
+        result = pd.read_sql_query(query,self.connection)
+        
+        return result
+    
+    
+    def getMostCommonCrimeType(self,area,lower,upper):
+        # gets the most common crime type for an area in a given year range
+        # area is the neighbourhood of interest
+        # lower is the lower bound year
+        # upper is the upper bound year
+        
+        query = '''SELECT name,cType,num
+                   FROM(
+                        SELECT Neighbourhood_Name as name,Crime_Type as cType, COUNT(Crime_Type) as num
+                        FROM crime_incidents
+                        WHERE name = '{}' AND Year <= {} AND Year >= {}
+                        GROUP BY name,Crime_Type)
+                   GROUP BY name
+                   HAVING MAX(num)'''.format(area,upper,lower)
+        result = pd.read_sql_query(query,self.connection)
+        return result
         
         
-        
-
-
 
 if __name__ == "__main__":
-    ui = UI()
+    # get tkinter window root
+    root = tk.Tk()
+    # set default window size
+    root.geometry('500x500')
+    # pass root UI and run
+    ui = UI(root)
     ui.run()
+    root.mainloop()
